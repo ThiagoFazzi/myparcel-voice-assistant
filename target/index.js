f647a418bdbd132fe7848f18591e6ba375702fcf
Xorifelse <johan.kruse.com>
2018-10-04 11:43:19 +0200

    Testing stream print

--- target/index.js
+++ target/index.js
@@ -1,14 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-<<<<<<< HEAD
-const routing_controllers_1 = require("routing-controllers");
 const controller_1 = require("./shipments/controller");
 const credential_1 = require("./shipments/credential");
-controller_1.getAccessToken(credential_1.credentialKeys);
-=======
-const controller_1 = require("./shipments/controller");
-const credential_1 = require("./shipments/credential");
->>>>>>> 1d12bb97999b14c460d0f0c367c7695d06089c3d
 exports.handler = (event, context) => {
     try {
         if (event.session.new) {
@@ -56,11 +49,5 @@ const generateResponse = (speechletResponse, sessionAttributes) => {
         response: speechletResponse
     };
 };
-<<<<<<< HEAD
-const port = process.env.PORT || 4000;
-const app = routing_controllers_1.createKoaServer({});
-app.listen(port, () => console.log(`Listening on port ${port}`));
-=======
 controller_1.getAccessToken(credential_1.credentialKeys);
->>>>>>> 1d12bb97999b14c460d0f0c367c7695d06089c3d
 //# sourceMappingURL=index.js.map
