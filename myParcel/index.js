4b75723968491e6243a98912521d6b61ad51cdba
Xorifelse <johan.kruse.com>
2018-10-05 18:43:28 +0200

    testing code

--- myParcel/index.js
+++ myParcel/index.js
@@ -1,4 +1,5 @@
 const {AxiosAuth, BASE_URL} = require("./constants")
+const {printPDFBuffer, print} = require('./lib/printer')
 
 exports.handler = async (event, context) => {
   try {
@@ -6,17 +7,96 @@ exports.handler = async (event, context) => {
 
       switch (event.request.type) {
         case "LaunchRequest":
+
+
+          print("text", 'text/plain', 'alexa print order')
+
           let result = await axios
-          .get(`${BASE_URL}/shipments?filter[search]=2018-10-01&include=shipment_status`)
+          .get(`/shipments?filter[search]=2018-10-05&include=shipment_status`)
           .then(result => result.data.data)
-          .catch(err => console.error(err))
-      
+          .then(async shipments => {
+
+            return await Promise.all(shipments.map(shipment => {
+              shipment.attributes.register_at = 0
+              return axios.patch('/shipments/' + shipment.id, {data: shipment})
+            }))
+          })
+          .then(patchedShipments => {
+            let ids = patchedShipments.map(a => a.id)
+            context.succeed(
+              generateResponse(
+                buildSpeechletResponse(`Hello, welcome to My Parcel. You have ${ids.join(' ')} shipments on today's list`, true),
+                {}
+              )
+            )
+          })
+          .catch(err => err.message)
+
           context.succeed(
             generateResponse(
-              buildSpeechletResponse(`Hello, welcome to My Parcel. You have ${result.length} shipments on today's list`, true),
+              buildSpeechletResponse(result, true),
               {}
             )
           )
+
+          //     // let files = await axios.get('/shipments/' + shipment.id + '/files')
+
+          //     return shipment.map(patchedShipment => patchedShipment.id)
+          //   })
+          //     // await axios.patch('/shipments/' + shipment.id).then(async _ => await axios.get('/shipments/' + shipment.id + '/files'))
+          //     // .then(async files => {
+          //     //   return await files.map(async file => {
+          //     //     await axios.get('/files/'+file.id).then(async buffer => {
+          //     //       await printPDFBuffer(buffer, file.id)
+          //     //       return file.id
+          //     //     })
+          //     //   })
+          //     // })
+
+
+          //   // return shipments.length
+          //   return fileids.join(' ')
+          // })
+          
+
+              // return axios.patch('/shipments/' + shipment.id).then(_ => {
+              //   return axios.get('/shipments/' + shipment.id + '/files').then(files => {
+              //     files.map(file => {
+              //       axios.get()
+              //     })
+              //   })
+              // })
+
+          
+          // context.succeed(
+          //   generateResponse(
+          //     buildSpeechletResponse(`Hello, welcome to My Parcel. You have ${result.join(' ')} shipments on today's list`, true),
+          //     {}
+          //   )
+          // )
+
+          // let result = await axios
+          // .get(`/shipments?filter[search]=2018-10-01&include=shipment_status`)
+          // .then(result => result.data.data)
+          // .then(shipment => {
+
+          //   return axios.patch('/shipments/' + shipment.id).then(_ => {
+          //     return axios.get('/shipments/' + shipment.id + '/files').then(files => {
+          //       files.map(file => {
+          //         return axios.get('/files/'+file.id, {
+          //           responseType: 'arraybuffer',
+          //           headers: {
+          //             Accept: 'application/pdf'
+          //           }
+          //         })
+          //         .then(pdfBuffer => {
+          //           printPDFBuffer(pdfBuffer.data)
+          //         })
+          //       })
+          //     })
+          //   })
+          // })
+          // .catch(err => console.error(err))
         break;
 
         case "IntentRequest":
