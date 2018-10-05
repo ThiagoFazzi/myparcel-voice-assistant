688dcafb652c9538a6662638936f85a1632ad040
Kristine <kristinetrona@gmail.com>
2018-10-05 11:15:50 +0200

    gettiung the number of shipments with alexa works

--- myParcel/index.js
+++ myParcel/index.js
@@ -1,13 +1,30 @@
 //import * as Alexa from "alexa-sdk"
 // import { getAccessToken } from './shipments/controller'
 // import { credentialKeys }  from './shipments/credential'
-// import * as request from 'superagent'
+const request = require("superagent")
+const Axios = require("axios")
+//import * as request from 'superagent'
 // import {printPDF} from './lib/printer'   UNCOMMENT THIS
-// import axois from 'Axios'                UNCOMMENT THIS
-
-
 //const file = './labels/testlabel.pdf'     UNCOMMENT THIS
 //printPDF(file) // comment out to avoid wasting paper.
+
+const BASE_URL = 'https://sandbox-api.myparcel.com/v1'
+const BASE_URL_AUTH = 'https://sandbox-auth.myparcel.com/access-token'
+const CREDENTIALS = {
+  "grant_type": "client_credentials",
+  "client_id": "5eb32787-07db-4898-91e4-68b1b24d6a1a",
+  "client_secret": "iah2Vg1uI6Q3i45Tq7UmjnA2J1Sse329bVRnVOE66ETk73ninmhYRac4RPng4KIy",
+  "scope": "*"
+}
+
+const AxiosAuth = async () => Axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: await Axios.post(BASE_URL_AUTH, CREDENTIALS).then(resp => `${resp.data.token_type} ${resp.data.access_token}`),
+    Accept: 'application/vnd.api+json',
+    ContentType: 'application/vnd.api+json'
+  }
+})
 
 exports.handler = async (event, context) => {
 
@@ -20,16 +37,19 @@ exports.handler = async (event, context) => {
 
       case "LaunchRequest":
 
-      // let result = await request
-      // .get("https://swapi.co/api/people/1/")
-      // .then(result => result.body.data.name)
-      // .catch(err => console.error(err))
+      await AxiosAuth().then(async axios => {
+      let result = await axios
+      .get(`${BASE_URL}/shipments?filter[search]=2018-10-01&include=shipment_status`)
+      .then(result => result.data.data)
+      .catch(err => console.error(err))
+      
         context.succeed(
           generateResponse(
-            buildSpeechletResponse(`Hello`, true),
+            buildSpeechletResponse(`Hello, you have ${result.length} orders today`, true),
             {}
+            )
           )
-        )
+        })
         break;
 
       case "IntentRequest":
