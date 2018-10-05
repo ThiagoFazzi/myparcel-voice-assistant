478eec188fddd2aa89b50d9bfe562f00e4206623
ThiagoFazzi <thiago@xtwo.com.br>
2018-10-05 18:42:37 +0200

    get buffer at axios

--- src/shipments/controller.ts
+++ src/shipments/controller.ts
@@ -30,37 +30,97 @@ export const getShipments = (axios, date) => {
 
 export const registerShipment = (axios, shipment) => {
   shipment.attributes.register_at = 0
+  const ship = {
+    data: shipment
+  }
   return axios
-    .patch(`${BASE_URL}/shipments/${shipment.id}`,{data: shipment})
+    .patch(`${BASE_URL}/shipments/${shipment.id}`,ship)
     .then(response =>  response.data)
     .catch(err => console.error(err))
 }
 
-export const getFile = (axios, shipmentId) => {
+export const getFile = (axios, shipment) => {
   return axios
-    .get(`${BASE_URL}/shipments/${shipmentId}/files`)
+    .get(`${BASE_URL}/shipments/${shipment}/files`)
     .then(response =>  response.data)
     .catch(err => console.error(err))
 }
 
 export const getContent = (axios, fileId) => {
-  return axios.get(`${BASE_URL}/files/${fileId}`)
-    .then(response => {/*printStreamPDF(Buffer.from(response.data, 'base64'))*/ })
+  return axios.get(`${BASE_URL}/files/${fileId}`, {
+    headers: {
+      Accept: 'application/pdf',
+      ContentType: 'application/pdf' 
+    }
+  })
+    //.then(response => response.data)
+    .then(response => { printPDFBuffer(Buffer.from(response.data, 'base64'))})
     .catch(err => console.log(err))
 }
 
 
 /*
-AxiosAuth().then(async axios => {
-  let shipments = await getShipments(axios)
-  shipments.data
-    .map(async shipment => {
-      await registerShipment(axios, shipment.id)
-    })  
-})
+export const getContent = (axios, fileId) => {
+  return axios.get(`${BASE_URL}/files/${fileId}`)
+    .then(response => {printStreamPDF(Buffer.from(response.data, 'base64')) })
+    .catch(err => console.log(err))
 */
 
 
 
+
+AxiosAuth().then(async axios => {
+  let shipments = await getShipments(axios,'2018-10-5')
+
+  //console.log(shipments)
+
+  let ShipmentsAfterPatch = <any> await Promise.all(shipments.data.map(shipment => registerShipment(axios, shipment)))
+  
+  let ShipmentsAfterPatch1 = <any> await Promise.all(ShipmentsAfterPatch.map(x=> getFile(axios, x.data.id)))
+
+  let ShipmentsAfterPatch2 = <any> await Promise.all(ShipmentsAfterPatch1.map(y => getContent(axios, y.data[0].id)))
+
+
+  //let shipmentsFile = await Promise.all(ShipmentsAfterPatch.map(shipmentFile => getFile(axios, shipmentFile)))
+  
+  //console.log(shipmentsFile)
+
+  //let shipmentsFileIds = shipmentsFile.map(shipmentContentFile => shipmentContentFile.data.id)
+  
+  //let shipmentsFileContents = await Promise.all(shipmentsFileIds.map(id => getContent(axios, id)))
+  //console.log(shipmentsFileContents)
+  //console.log('shipmentsFile',shipmentsFile)
+  //console.log('shipmentsFileContent',shipmentsFileContent)
+
+  
+  //let filesId = shipments.map(shipmentFile => getFile(axios,shipmentFile.id))
+  //filesId.map(shipmentFileId => getContent(axios, shipmentFileId))
+})
+
+
+
+
+/*
+let result = await axios
+          .get(`/shipments?filter[search]=2018-10-05&include=shipment_status`)
+          .then(result => result.data.data)
+          .then(async shipments => {
+            let fileids = await shipments.map(async shipment => {
+              await axios.patch('/shipments/' + shipment.id).then(_ => axios.get('/shipments/' + shipment.id + '/files'))
+              .then(files => {
+                return files.map(async file => {
+                  await axios.get('/files/'+file.id).then(async buffer => {
+                    await printPDFBuffer(buffer, file.id)
+                    return file.id
+                  })
+                })
+              })
+            })
+
+            // return shipments.length
+            return fileids.join(' ')
+          })
+
+*/
 
 
