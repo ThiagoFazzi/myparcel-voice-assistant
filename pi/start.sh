5f62b057e4c64f6611843bf8bf6220933e10765b
Xorifelse <johan.kruse.com>
2018-10-07 14:44:31 +0200

    Configured TS

--- pi/start.sh
+++ pi/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+DATABASE_URL = 'postgres://myparcel:secret@localhost:5432/labels'
+PORT = 8080
+
 cd ~/
+rm -rf myparcel-voice-assistant
 git clone https://github.com/JoyceTerra/myparcel-voice-assistant.git
 cd ~/myparcel-voice-assistant/pi
 x-terminal-emulator -e yarn start
