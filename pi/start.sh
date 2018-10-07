4aca3ba281d56554e340501fd650619ad86dc0af
Xorifelse <johan.kruse.com>
2018-10-07 18:58:01 +0200

    Fixed issue of ssh timeout on remote server

--- pi/start.sh
+++ pi/start.sh
@@ -4,4 +4,4 @@ cd ~/myparcel-voice-assistant/pi
 git pull origin pi-server
 x-terminal-emulator -e yarn compile
 x-terminal-emulator -e yarn start
-x-terminal-emulator -e ssh -R myparcel.serveo.net:80:localhost:4000 serveo.net
+x-terminal-emulator -e autossh -M 0 -R myparcel.serveo.net:80:localhost:4000 serveo.net
