3ec30aa4e1b002595b1ada4f087b87d003f127b6
Xorifelse <johan.kruse.com>
2018-10-07 16:37:38 +0200

    power on script

--- pi/start.sh
+++ pi/start.sh
@@ -3,9 +3,8 @@
 DATABASE_URL = 'postgres://myparcel:secret@localhost:5432/labels'
 PORT = 8080
 
-cd ~/
-rm -rf myparcel-voice-assistant
-git clone https://github.com/JoyceTerra/myparcel-voice-assistant.git
 cd ~/myparcel-voice-assistant/pi
+git pull origin pi-server
+x-terminal-emulator -e yarn compile
 x-terminal-emulator -e yarn start
 x-terminal-emulator -e ssh -R myparcel.serveo.net:80:localhost:8080 serveo.net
