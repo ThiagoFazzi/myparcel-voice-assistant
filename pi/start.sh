042afdaa325172ba354ef26db3ad697eb54600a6
Xorifelse <johan.kruse.com>
2018-10-07 17:10:31 +0200

    Updated port

--- pi/start.sh
+++ pi/start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-DATABASE_URL = 'postgres://myparcel:secret@localhost:5432/labels'
-PORT = 8080
-
 cd ~/myparcel-voice-assistant/pi
 git pull origin pi-server
 x-terminal-emulator -e yarn compile
 x-terminal-emulator -e yarn start
-x-terminal-emulator -e ssh -R myparcel.serveo.net:80:localhost:8080 serveo.net
+x-terminal-emulator -e ssh -R myparcel.serveo.net:80:localhost:4000 serveo.net
